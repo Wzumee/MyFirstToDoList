c6394913abcc9adac1e0e83aa20d54e7959382e3
ADMIN <ADMIN@GRAVITRON>
2025-07-15 11:53:03 +0700

    Add project files.

---
+++ Test01/MethodClass.cs
@@ -0,0 +1,113 @@
+﻿using MainProgram;
+using Newtonsoft.Json;
+
+namespace SubProgram
+{
+    class MethodClass
+    {
+        public static List<string> todoList = new List<string>();
+        public static void LoadTodos()
+        {
+            if (File.Exists("todos.json"))
+            {
+                string readToDo = File.ReadAllText("todos.json"); 
+                todoList = JsonConvert.DeserializeObject<List<string>>(readToDo); 
+            }
+        }
+        public static void SaveTodos()
+        {
+            string json = JsonConvert.SerializeObject(todoList, Formatting.Indented); 
+            File.WriteAllText("todos.json", json);
+        }
+        public static void Wait2sec()
+        {
+            Thread.Sleep(2000);
+        }
+        public static void ClearLastLine()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop);
+        }
+        public static void SeeAllTodo()
+        {
+            if (StartScreen.SeeToDo)
+            {
+                Console.WriteLine("-----Here are your current TODOs-----");
+                Console.WriteLine(todoList.Count == 0
+                    ? "\nNo TODOs to display."
+                    : string.Join("\n", todoList.Select((todo, i) => $"\n{i + 1}. {todo}")));
+                Console.WriteLine(" ");
+            }
+        }
+        public static void AddTodo()
+        {
+            Console.Clear();
+            Console.WriteLine("-----Here are your current TODOs-----");
+            for (int i = 0; i < todoList.Count; i++)
+            {
+                Console.WriteLine($"\n{i + 1}. {todoList[i]}");
+            }
+            Console.Write("\nReference: .............. xx/xx/xxxx xx:xx" +
+                "\n" +
+                "\nEnter a new TODO:");
+            string newTodo = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(newTodo) || todoList.Contains(newTodo))
+            {
+                MethodClass.ClearLastLine();
+                Console.Write(string.IsNullOrWhiteSpace(newTodo) ? "ToDo Can't be NullorWhiteSpace: " : "ToDo Already Exist: ");
+                newTodo = Console.ReadLine();
+            }
+            todoList.Add(newTodo);
+            Console.WriteLine("TODO added successfully.");
+            Console.WriteLine("\nPress any key to return to the main menu...");
+            
+            Console.ReadKey();
+        }
+        public static void RemoveToDo()
+        {
+            Console.Clear();
+            if (todoList.Count == 0)
+            {
+                Console.WriteLine("No TODOs to remove.");
+            }
+            else
+            {
+                Console.WriteLine("-----Here are your current TODOs-----");
+                for (int i = 0; i < todoList.Count; i++)
+                {
+                    Console.WriteLine($"\n{i + 1}. {todoList[i]}");
+                }
+                Console.Write("\nEnter a Number/All To Remove: ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "all")
+                {
+                    todoList.Clear();
+                    Console.WriteLine("All ToDo it Removed...");
+                    Wait2sec();
+                    return;
+                }
+                while (!int.TryParse(input, out int number) || number < 1 || number > todoList.Count)
+                {
+                    ClearLastLine();
+                    Console.Write("Invalid input Please try again: ");
+                    Wait2sec();
+                    input = Console.ReadLine();
+                    if (input.ToLower() == "all")
+                    {
+                        todoList.Clear();
+                        Console.WriteLine("All ToDo is removed...");
+                        Wait2sec();
+                        return;
+                    }
+                }
+                int todoNumber = int.Parse(input);
+                string removeTodo = todoList[todoNumber - 1];
+                todoList.RemoveAt(todoNumber - 1);
+                Console.WriteLine($"\nRemove ToDo: {todoNumber}  \"{removeTodo}\"");
+            };
+            Console.WriteLine("\nPress any key to return to the main menu...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test01/Startscreen.cs
@@ -0,0 +1,84 @@
+﻿using SubProgram;
+
+namespace MainProgram
+{
+    class StartScreen
+    {
+        public static bool SeeToDo = false;
+
+        static void Main(string[] args)
+        {
+            MethodClass.LoadTodos();
+
+            bool KeepOpening = true;
+            while (KeepOpening == true)
+            {
+                Console.Clear();
+                MethodClass.SeeAllTodo();
+                if (SeeToDo == true)
+                {
+                    Console.WriteLine("----------ToDo list Program----------" +
+                    "\n" +
+            "\nWhat do you want to do?" +
+            "\n" +
+            "\n[1]Hide all TODO" +
+            "\n[2]Add a TODO" +
+            "\n[3]Remove a TODO" +
+            "\n[4]Exit");
+                }
+                else
+                {
+                    Console.WriteLine("-----Welcome to ToDo list Program-----" +
+                        "\n" +
+                "\nWhat do you want to do?" +
+                "\n" +
+                "\n[1]See all TODO" +
+                "\n[2]Add a TODO" +
+                "\n[3]Remove a TODO" +
+                "\n[4]Exit");
+                }
+                Console.Write("Pick Your Choice: ");
+                var choice = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(choice))
+                {
+                    MethodClass.ClearLastLine();
+                    do
+                    {
+                        Console.Write("Choice Can't Be NullOrWhiteSpace: ");
+                        choice = Console.ReadLine();
+                        MethodClass.ClearLastLine();
+                    }
+                    while (string.IsNullOrWhiteSpace(choice));
+                }
+                MethodClass.ClearLastLine();
+                switch (choice)
+                {
+                    case "1":
+                        if (SeeToDo == true)
+                        {
+                            SeeToDo = false;
+                            break;
+                        }
+                        SeeToDo = true;
+                        break;
+                    case "2":
+                        MethodClass.AddTodo();
+                        break;
+                    case "3":
+                        MethodClass.RemoveToDo();
+                        break;
+                    case "4":
+                        KeepOpening = false;
+                        Console.WriteLine("Program is Closing.....");
+                        MethodClass.Wait2sec();
+                        return;
+                    default:
+                        Console.WriteLine("Invalid Input....");
+                        MethodClass.Wait2sec();
+                        break;
+                }
+                MethodClass.SaveTodos();
+            }
+        }
+    }
+}
